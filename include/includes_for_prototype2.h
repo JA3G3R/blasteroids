3b12fbefc2d39f3bf469ba8e5ec5e362121ca7ea
Bhavarth Karmarkar <bhavarth1905kr@gmail.com>
2021-08-22 20:25:05 -0700

    Ubuntu commit #2

--- include/includes_for_prototype2.h
+++ include/includes_for_prototype2.h
@@ -7,22 +7,45 @@
 #include "../include/menu_functions.h"
 #include "./include_allegro.h"
 #include "./collision.h"
+/* defines */
+#define OMEGA   0.10
+#define SC_WIDTH        1200
+#define SC_HEIGHT       650
+#define ACC     0.1
+#define DECC    0.5
+#define MAX_SPEED       10
+#define SCALE   25
+#define SPEED_X 8
+#define SPEED_Y 4
+
+/* flags */
+#define GAME_STARTED     0b00000001 //this indicated that a game has been started by the player
+#define GAME_PAUSED      0b00000010 //this indicated that the player is in menu
+#define PROT_SP          0b00000100 //this is used to protect the sp from asteroids
+#define NEW_GAME_STARTED 0b00001000 //this indicates that a fresh game has been started by the player
+#define SP_RESPAWN	 0b00010000 //this indicates that the spaceship has respawned after impact from an asteroid
+#define RESPAWNING	 0b00100000 //this indicates that the player is in respawning screen
+#define SP_DEAD		 0b01000000 //this indicates that all the lives are used by the spaceship and its dead
 typedef struct blast blast;
-typedef struct blast{
+typedef struct blast
+{
+        bool alive;
         float x;
         float y;
         float speed;
-        bool alive;
         float direction;
         ALLEGRO_TRANSFORM* trans;
         blast* next;
 }blast;
+
 typedef struct game_t
 {
         int state;
         int blasts_on_screen;
         int asteroids_on_screen;
+	int asteroids_alive;
         int score;
+	int lives_left;
         ALLEGRO_TIMER* timer;
         ALLEGRO_EVENT_QUEUE* queue;
         ALLEGRO_EVENT event;
@@ -33,10 +56,14 @@ typedef struct game_t
 
 void draw_each_blast();
 void generate_blast(float sp_pos_x,float sp_pos_y,float direction);
-void handle_sp_collision(float sp_x,float sp_y,float theta);
+void handle_sp_collision(float *sp_x,float *sp_y,float *theta);
 void handle_collision();
 asteroid* generate_asteroids();
 void draw_each_asteroid();
-void clean_blasts();
+void clean_blasts(bool );
+void clean_asteroids(bool );
+void* generate_new_asteroids(void* null);
 void generate_broken_asteroids(asteroid* broken,float x_multiplier,float y_multiplier);
 void break_asteroid(asteroid *as,blast *bl);
+void respawn_wait();
+void you_died();
--- lib/prototype_funcs.c
+++ lib/prototype_funcs.c
@@ -1,27 +1,14 @@
 #include "../include/includes_for_prototype2.h"
-#define SC_HEIGHT	650
-#define SC_WIDTH	1200
-#define ACC     0.1
-#define DECC    0.5
-#define MAX_SPEED       10
-#define SCALE   25
-#define SPEED_X 3
-#define SPEED_Y 4
-#define OMEGA	0.12
 game_t* game;
-pthread_mutex_t lock_count_main=PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_count_collider=PTHREAD_MUTEX_INITIALIZER;
+extern ALLEGRO_TRANSFORM *trans;
+ALLEGRO_TRANSFORM local_trans;
+ALLEGRO_FONT* local_font; 
+//ALLEGRO_TRANSFORM death_trans; 
+
 void generate_broken_asteroids(asteroid* broken,float x_multiplier,float y_multiplier){
         asteroid* smaller1=(asteroid* )(malloc(sizeof(asteroid)));
         asteroid* smaller2=(asteroid* )(malloc(sizeof(asteroid)));
-        asteroid* next=game->as_null->next;
-        pthread_mutex_lock(&lock_count_main);
-        pthread_mutex_lock(&lock_count_collider);
-        game->as_null->next=smaller1;
-        smaller1->next=smaller2;
-        smaller2->next=next;
-       	game->asteroids_on_screen+=2;
-
+        
         float speed=sqrt(broken->speed_x*broken->speed_x + broken->speed_y*broken->speed_y);
 
         //smaller1 
@@ -32,7 +19,7 @@ void generate_broken_asteroids(asteroid* broken,float x_multiplier,float y_multi
                 smaller1->current_rotation=broken->current_rotation+0.5236;
                 smaller1->speed_x=speed*x_multiplier;
                 smaller1->speed_y=speed*y_multiplier;
-                smaller1->color=al_map_rgb(255,255,255);
+                smaller1->color=al_map_rgb(0,0,255);
 		smaller1->trans=(ALLEGRO_TRANSFORM*)(malloc(sizeof(ALLEGRO_TRANSFORM)));
                 al_identity_transform(smaller1->trans);
                 smaller1->x=broken->x+smaller1->speed_x*smaller1->scale*25;
@@ -48,16 +35,20 @@ void generate_broken_asteroids(asteroid* broken,float x_multiplier,float y_multi
                 smaller2->current_rotation=broken->current_rotation-0.5236;
                 smaller2->speed_x=-speed*x_multiplier;
                 smaller2->speed_y=-speed*y_multiplier;
-                smaller2->color=al_map_rgb(255,255,255);
+                smaller2->color=al_map_rgb(0,0,255);
                 smaller2->trans=(ALLEGRO_TRANSFORM*)(malloc(sizeof(ALLEGRO_TRANSFORM)));
                 al_identity_transform(smaller2->trans);
                 smaller2->x=broken->x+smaller2->speed_x*smaller2->scale*25;
                 smaller2->y=broken->y+smaller2->speed_y*smaller2->scale*25;
 		        }
-        pthread_mutex_unlock(&lock_count_collider);
-        pthread_mutex_unlock(&lock_count_main);
-}
 
+        asteroid* next=game->as_null->next;
+	smaller1->next=smaller2;
+        smaller2->next=next;
+        game->as_null->next=smaller1;
+       	game->asteroids_on_screen+=2;
+}
+	
 void break_asteroid(asteroid *as,blast *bl){
         float as_speed=sqrt((as->speed_x*as->speed_x)+(as->speed_y*as->speed_y));
         float bl_speed_x=-(bl->speed*cos(1.57+bl->direction));
@@ -67,7 +58,6 @@ void break_asteroid(asteroid *as,blast *bl){
         generate_broken_asteroids(as,x_multiplier,y_multiplier);
 }
 
-
 void draw_each_blast(){
         blast* ptr = game->bl_null;
         for(int i=0;i<game->blasts_on_screen;i++){
@@ -75,14 +65,18 @@ void draw_each_blast(){
 		if(ptr->x == 0 || ptr->x== SC_WIDTH ||ptr->y==0||ptr->y == SC_HEIGHT)
                         ptr->alive=false;
                 if(ptr->alive){
+			if(!(game->state & RESPAWNING))//we want that when respawning sp everything on the screen freezes(the following block of code updates the postion of blasts)
+			{
                 	ptr->x=translate_spaceship_x(ptr->trans,ptr->speed,SC_WIDTH,ptr->direction,ptr->x);
                 	ptr->y=translate_spaceship_y(ptr->trans,ptr->speed,SC_HEIGHT,ptr->direction,ptr->y);
+			}
                 	al_use_transform(ptr->trans);
-                	al_draw_line(0,0,1,0,al_map_rgb(0,255,0),3.0f);
+                	al_draw_line(0,0,2,0,al_map_rgb(255,0,255),7.0f);
                 }
         }
 
 }
+
 void generate_blast(float sp_pos_x,float sp_pos_y,float direction){
         blast* ptr=(blast*)(malloc(sizeof(blast)));
         ptr->x=sp_pos_x;
@@ -97,36 +91,49 @@ void generate_blast(float sp_pos_x,float sp_pos_y,float direction){
         ptr->next=game->bl_null->next;
         game->bl_null->next=ptr;
 }
-void handle_sp_collision(float sp_x,float sp_y,float theta){
+
+void handle_sp_collision(float *sp_x,float *sp_y,float *theta){
         asteroid* ptr=game->as_null;
-        for(int i=0;i<game->asteroids_on_screen;i++){
+	int itr=game->asteroids_on_screen;
+        for(int i=0;i<itr;i++){
+
                 ptr=ptr->next;
                 if(!ptr->alive) continue;
 
-                if(is_spaceship_hit(sp_x,sp_y,ptr,theta,SCALE)){
+                if(is_spaceship_hit(*sp_x,*sp_y,ptr,*theta,SCALE)){
 
                         ptr->speed_x *=-1;
                         ptr->speed_y *= -1;
 
-                        printf("YOU DIED\n");
-                        while(is_spaceship_hit(sp_x,sp_y,ptr,theta,SCALE)){
-				int x=getuid();
+                        game->state |= SP_RESPAWN;
+			game->state |= PROT_SP;
+			game->lives_left--;
+			*sp_x=SC_WIDTH/2;
+			*sp_y=SC_HEIGHT/2;
+			*theta=0;
+			al_identity_transform(trans);
+			al_translate_transform(trans,*sp_x,*sp_y);
+			game->state |= RESPAWNING;
+                        while(is_spaceship_hit(*sp_x,*sp_y,ptr,*theta,SCALE)){
                                 translate_asteroid_x(ptr->trans,SC_WIDTH,&(ptr->x),&(ptr->speed_x));
                                 translate_asteroid_y(ptr->trans,SC_HEIGHT,&(ptr->y),&(ptr->speed_y));
-
                         }
                 }
         }
 }
+
 void handle_collision(){
         asteroid* ptr;
         asteroid*ptr1=game->as_null;
-        for(int i=0;i<game->asteroids_on_screen-1;i++){
+	int iter=game->asteroids_on_screen-1;
+        for(int i=0;i<iter;i++){
+
                 ptr1=ptr1->next;
                 if(!ptr1->alive) continue;
 
                 ptr=ptr1;
-                for(int j=0;j<game->asteroids_on_screen-(i+1);j++){
+		int itr = game->asteroids_on_screen-(i+1);
+                for(int j=0;j<itr;j++){
                         ptr=ptr->next;
                         if(!ptr->alive) continue;
                         if(is_collision_detected(ptr,ptr1))
@@ -135,6 +142,7 @@ void handle_collision(){
         }
 
 }
+
 asteroid* generate_asteroids(){
         srand(time(0));
         asteroid* as_null = (asteroid* )malloc(sizeof(asteroid));
@@ -161,25 +169,32 @@ asteroid* generate_asteroids(){
                 ptr->alive=true;
                 ptr->current_rotation=(rand()%20)*0.01+0.05;
                 ptr->omega=ptr->current_rotation+0.05;
-                ptr->color = al_map_rgb(255,255,255);
+                ptr->color = al_map_rgb(0,0,255);
                 ptr->trans = (ALLEGRO_TRANSFORM*)(malloc(sizeof(ALLEGRO_TRANSFORM)));
                 al_identity_transform((ptr->trans));
         }
         return as_null;
 }
+
 void draw_each_asteroid(){
-        asteroid* ptr = game->as_null;
-        for(int i=0;i<game->asteroids_on_screen;i++){
+       asteroid* ptr = game->as_null;
+       int itr = game->asteroids_on_screen;
+        for(int i=0;i<itr;i++){
                 ptr=ptr->next;
                 if(ptr->alive){
-                        al_identity_transform(ptr->trans);
-			al_scale_transform(ptr->trans,ptr->scale,ptr->scale);
-                        translate_asteroid_x(ptr->trans,SC_WIDTH,&(ptr->x),&(ptr->speed_x));
-                        translate_asteroid_y(ptr->trans,SC_HEIGHT,&(ptr->y),&(ptr->speed_y));
-                        rotate_object(ptr->trans,ptr->x,ptr->y,ptr->current_rotation);
-                        ptr->current_rotation+=ptr->omega;
-                        if(ptr->current_rotation>3.14159*2)
-                        ptr->current_rotation-=(3.14159*2);
+			if(!(game->state & RESPAWNING)){
+				// the following block of code updates the position of asteroids
+				// and to implement respawn_wait we need that everything on the screen
+				// freezes( i.e the asteroid positions don't get updated
+                        	al_identity_transform(ptr->trans);
+				al_scale_transform(ptr->trans,ptr->scale,ptr->scale);
+                        	translate_asteroid_x(ptr->trans,SC_WIDTH,&(ptr->x),&(ptr->speed_x));
+                        	translate_asteroid_y(ptr->trans,SC_HEIGHT,&(ptr->y),&(ptr->speed_y));
+                        	rotate_object(ptr->trans,ptr->x,ptr->y,ptr->current_rotation);
+                        	ptr->current_rotation+=ptr->omega;
+                        	if(ptr->current_rotation>3.14159*2)
+                        	ptr->current_rotation-=(3.14159*2);
+			}
                         al_use_transform(ptr->trans);
                         al_draw_line(-20, 20, -25, 5, ptr->color, 2.0f);
                         al_draw_line(-25, 5, -25, -10, ptr->color, 2.0f);
@@ -196,7 +211,8 @@ void draw_each_asteroid(){
                 }
         }
 }
-void clean_blasts(){
+
+void clean_blasts(bool free_all){
         blast* prev=game->bl_null;
         blast* curr=game->bl_null;
         blast* nxt=game->bl_null->next;
@@ -207,16 +223,190 @@ void clean_blasts(){
                         prev=prev->next;
                 if(i!=itr-1)
                         nxt=nxt->next;
-                if(!(curr->alive)){
+                if(!(curr->alive) || free_all){
+                        game->blasts_on_screen-=1;
                         if(i!=itr-1)
                                 prev->next=nxt;
                         else prev->next=NULL;
                         free(curr->trans);
                         free(curr);
                         curr=NULL;
-                        game->blasts_on_screen-=1;
                 }
         }
 }
 
+void clean_asteroids(bool free_all){
+        asteroid* prev=game->as_null;
+        asteroid* curr=game->as_null;
+	asteroid* nxt=game->as_null->next;
+        int itr= game->asteroids_on_screen;
+        for(int i=0;i<itr;i++){
+                curr=nxt;
+                if(prev->next!=nxt)
+                        prev=prev->next;
+                if(i!=itr-1)
+                        nxt=nxt->next;
+                if(!(curr->alive) || free_all){
+                        game->asteroids_on_screen-=1;
+                        if(i!=itr-1)
+                                prev->next=nxt;
+                        else prev->next=NULL;
+                        free(curr->trans);
+                        free(curr);
+                        curr=NULL;
+                }
+        }
+}
 
+void* generate_new_asteroids(void *null){
+	while(1){
+	int itr=game->asteroids_on_screen;
+	if(itr <= 2){
+	srand(time(0));
+	asteroid* ptr;
+        for(int i=0;i < 5; i++){
+                ptr= (asteroid* )(malloc(sizeof(asteroid)));
+                int X=rand()%2;
+                int Y=rand()%2;
+                int is_negative_X = 1;
+                int is_negative_Y = 1;
+                for(int i=0;i<=X;i++){
+                         is_negative_X*=-1;
+                 }
+                for(int i=0;i<=Y;i++){
+                        is_negative_Y*=-1;
+                }
+                ptr->speed_x = ((float)(rand())/(float)(RAND_MAX))*SPEED_X*is_negative_X;
+                ptr->speed_y=((float)(rand())/(float)(RAND_MAX))*SPEED_Y*is_negative_Y;
+		X=rand()%4;
+		switch(X){
+			case 0:
+				ptr->x = (float)(rand()%SC_WIDTH);
+				ptr->y = -23;
+				break;
+			case 1:
+				ptr->x =  (float)(rand()%SC_WIDTH);
+				ptr->y = SC_HEIGHT+23;
+				break;
+			case 2:
+				ptr->x = -23;
+				ptr->y= (float)(rand()%SC_HEIGHT);
+				break;
+			case 3:
+				ptr->x = SC_WIDTH + 23;
+				ptr->y = (float)(rand()%SC_HEIGHT);
+				break;
+		}
+		ptr->scale=1;
+                ptr->alive=true;
+                ptr->current_rotation=(rand()%20)*0.01+0.05;
+                ptr->omega=ptr->current_rotation+0.05;
+                ptr->color = al_map_rgb(0,0,255);
+                ptr->trans = (ALLEGRO_TRANSFORM*)(malloc(sizeof(ALLEGRO_TRANSFORM)));
+                al_identity_transform((ptr->trans));
+
+		ptr->next=game->as_null->next;
+		game->as_null->next=ptr;
+		game->asteroids_on_screen++;
+
+        }
+	}
+}
+}
+
+void respawn_wait(){
+	int wait_timer=0;
+	int time_remaining=3;
+	char string[20];
+	local_font= al_create_builtin_font();
+	if(!local_font){
+		printf("local_font variable can't be initialized\n");
+		exit(1);
+	}
+	ALLEGRO_EVENT_QUEUE* local_queue=al_create_event_queue();
+	ALLEGRO_TIMER* timer_loc=al_create_timer(1.0/30.0);
+	ALLEGRO_EVENT local_event;
+	al_register_event_source(local_queue,al_get_timer_event_source(timer_loc));
+
+	al_identity_transform(&local_trans);
+	al_scale_transform(&local_trans,2,2);
+	al_translate_transform(&local_trans,SC_WIDTH/2,SC_HEIGHT/2);
+	al_use_transform(&local_trans);
+//	al_flush_event_queue(game->queue);
+	sprintf(string,"Respawning in ... %d",time_remaining);
+	al_start_timer(timer_loc);
+	while(1){	
+		
+		al_wait_for_event(local_queue,&(local_event));
+		
+		if(wait_timer==30){
+			time_remaining--;
+			if(time_remaining== 0){	
+				al_destroy_font(local_font);
+				al_destroy_event_queue(local_queue);
+				al_destroy_timer(timer_loc);
+				break;
+			}
+			wait_timer=0;
+			sprintf(string,"Respawning in ... %d",time_remaining);
+		}
+		if(game->event.type==ALLEGRO_EVENT_TIMER){
+			wait_timer++;
+			al_clear_to_color(al_map_rgb(0,0,0));
+			al_draw_text(local_font,al_map_rgb(255,255,255),0,0,ALLEGRO_ALIGN_CENTRE,string);
+			al_flip_display();
+			
+		}
+		
+	}
+	game->state &= ~(RESPAWNING);
+	al_flush_event_queue(game->queue);
+}
+
+void you_died(){
+	int wait_timer=0;
+	int time_remaining=3;
+	char string[22];
+	local_font= al_create_builtin_font();
+	if(!local_font){
+		printf("local_font variable can't be initialized\n");
+		exit(1);
+	}
+	ALLEGRO_EVENT_QUEUE* local_queue=al_create_event_queue();
+	ALLEGRO_TIMER* timer_loc=al_create_timer(1.0/30.0);
+	ALLEGRO_EVENT local_event;
+	al_register_event_source(local_queue,al_get_timer_event_source(timer_loc));
+	ALLEGRO_FONT* death_font=al_create_builtin_font();
+
+	al_identity_transform(&death_trans);
+	al_scale_transform(&death_trans,2,2);
+	al_use_transform(&death_trans);
+	sprintf(string,"Back to menu in ... %d",time_remaining);
+	al_start_timer(timer_loc);
+	while(1){	
+		
+		al_wait_for_event(local_queue,&(local_event));
+		
+		if(wait_timer==30){
+			time_remaining--;
+			if(time_remaining== 0){	
+				al_destroy_font(death_font);
+				al_destroy_event_queue(local_queue);
+				al_destroy_timer(timer_loc);
+				break;
+			}
+
+			sprintf(string,"Back to menu in ... %d",time_remaining);
+		}
+		if(game->event.type==ALLEGRO_EVENT_TIMER){
+			wait_timer++;
+			al_clear_to_color(al_map_rgb(0,0,0));
+			al_draw_text(local_font,al_map_rgb(255,255,255),SC_WIDTH/2,SC_HEIGHT/2,ALLEGRO_ALIGN_CENTRE,string);
+			al_draw_text(local_font,al_map_rgb(255,255,255),SC_WIDTH/2,SC_HEIGHT/2+15,ALLEGRO_ALIGN_CENTRE,"YOU DIED !!!");
+			al_flip_display();
+			
+		}
+		
+	}
+	al_flush_event_queue(game->queue);
+}
